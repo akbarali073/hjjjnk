c610d0ea3c2b85ef1a4503ca2e8760262eba0c9d
akbarali073 <1winakg@gmail.com>
2026-02-07 18:37:16 +0500

    Update print statement from 'Hello' to 'Goodbye'

--- bot.js
+++ bot.js
@@ -277,7 +277,7 @@ const getChannelMarkup = () => ({
     [
       {
         text: "➕ 1 - Qo'shilish",
-        url: "https://t.me/patrickstarsrobot?start=907402803",
+        url: "https://t.me/halol_ton_oling",
       }
     ],
     [
@@ -508,6 +508,7 @@ bot.on("callback_query", async (query) => {
 
 process.on("uncaughtException", (err) => console.log("Kritik xato:", err));
 console.log("🔥 Bot barcha URL'lar bilan xatosiz ishga tushdi!");
+
 
 
 
